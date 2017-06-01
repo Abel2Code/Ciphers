81f40ddb56282be2fcc101b212c0f953bcce6889
Abel2Code <asalinaswc1@aol.com>
2017-05-31 18:35:51 -0700

    Converted code to methods

--- CeaserCipher/CeaserCipher.py
+++ CeaserCipher/CeaserCipher.py
@@ -1,3 +1,24 @@
+def validPassword(password):
+    if password.strip().isnumeric():
+        return True
+    return False
+
+def configureCipher(password):
+    for i in range(int(password)):
+        cipher.insert(0, cipher[len(cipher) - 1])  # inserts last character of string to the front of the array
+        cipher.pop()  # removes last character in array
+
+def encryptionDecision(validInput = False):
+    while not validInput:
+        options = input('\n'
+                        'To encrypt a message, type ENCRYPT.\n'
+                        'To decrypt a message, type DECRYPT\n').strip().lower()
+        if options == 'encrypt' or options == 'decrypt':
+            validInput = True
+            return options
+        else:
+            print('ERROR: PLEASE TRY AGAIN')
+#End Of Functions
 print('Greetings User!\n'
       'This program will encrypt your input. To decrypt it, you must remember the password.\n'
       'This program creates a caesar cipher.\n')
@@ -11,26 +32,17 @@ cipher = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
 isValidPassword = False
 while not isValidPassword:
     password = input('Input a number. Remember this password to decrypt your message later.\n')
-    if password.strip().isnumeric():
+    if validPassword(password):
         # isnumeric() is a boolean method that checks whether a String is a number
         # strip() removes spaces at beginning and end of string
         isValidPassword = True
 
 interval = 0
 #This loop creates the cipher
-for i in range(int(password)):
-    cipher.insert(0, cipher[len(cipher)-1]) # inserts last character of string to the front of the array
-    cipher.pop() # removes last character in array
+configureCipher(password)
 
-validInput = False
-while not validInput:
-    options = input('\n'
-                    'To encrypt a message, type ENCRYPT.\n'
-                    'To decrypt a message, type DECRYPT\n').strip().lower()
-    if options == 'encrypt' or options == 'decrypt':
-        validInput = True
-    else:
-        print('ERROR: PLEASE TRY AGAIN')
+
+options = encryptionDecision()
 
 message = input('\nInput the text you would like to ' + options + ':\n')
 
